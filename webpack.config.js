720733f90c1660378352f12a578f5c0e5267136e
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-04-12 23:06:05 +0900

    コンパイルテスト

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+const outputPath = path.resolve(__dirname, "dist")
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: outputPath
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: outputPath
+  }
+};
