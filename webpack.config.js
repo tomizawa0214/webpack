36ef02d45ffbff4f92b6cf7002a17536288acbe2
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-04-26 14:38:14 +0900

    config変更

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,54 @@
 const path = require("path");
 
-const outputPath = path.resolve(__dirname, "dist")
+const outputPath = path.resolve(__dirname, "../tomizawa/docs")
 
 module.exports = {
-  entry: "./src/index.js",
+  mode: "production",
+  devtool: "source-map",
+
+  entry: "../tomizawa/docs/index.js",
   output: {
     filename: "main.js",
     path: outputPath
   },
+
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              url: true,
+              sourceMap: true,
+              importLoaders: 2
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              sourceMap: true,
+              plugins: [
+                require("autoprefixer")({
+                  grid: true
+                })
+              ]
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(gif|png|jpg|eot|wof|woff|woff2|ttf|svg)$/,
+        loader: "url-loader"
       }
     ]
-  },
-  devServer: {
-    contentBase: outputPath
   }
 };
